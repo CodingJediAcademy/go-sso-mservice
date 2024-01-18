caf11399b1f789ba62328ad6c308c177be364f90
jedi <coding.jedi.knight@gmail.com>
2024-01-18 21:29:24 +0300

    (add)[auth]{login method}

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,12 +2,26 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/CodingJediAcademy/go-sso-mservice/internal/domain/models"
+	"github.com/CodingJediAcademy/go-sso-mservice/internal/lib/jwt"
 	"github.com/CodingJediAcademy/go-sso-mservice/internal/lib/logger/sl"
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"time"
+)
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
 
 type UserSaver interface {
@@ -26,14 +40,6 @@ type AppProvider interface {
 	App(ctx context.Context, appID int) (models.App, error)
 }
 
-type Auth struct {
-	log         *slog.Logger
-	usrSaver    UserSaver
-	usrProvider UserProvider
-	appProvider AppProvider
-	tokenTTL    time.Duration
-}
-
 func New(
 	log *slog.Logger,
 	usrSaver UserSaver,
@@ -48,6 +54,65 @@ func New(
 		appProvider: appProvider,
 		tokenTTL:    tokenTTL,
 	}
+}
+
+// Login checks if user with given credentials exists in the system and returns access token.
+//
+// If user exists, but password is incorrect, returns error.
+// If user doesn't exist, returns error.
+func (a *Auth) Login(
+	ctx context.Context,
+	email string,
+	password string,
+	appID int,
+) (string, error) {
+	const op = "Auth.Login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("username", email),
+		// password либо не логируем, либо логируем в замаскированном виде
+	)
+
+	log.Info("attempting to login user")
+
+	// Достаём пользователя из БД
+	user, err := a.usrProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", sl.Err(err))
+
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+
+		a.log.Error("failed to get user", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	// Проверяем корректность полученного пароля
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		a.log.Info("invalid credentials", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	// Получаем информацию о приложении
+	app, err := a.appProvider.App(ctx, appID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user logged in successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to generate token", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
 }
 
 // RegisterNewUser registers new user in the system and returns user ID.
