df4df671e80cd59fb5d0c00cace2ef9feecec4c9
jedi <coding.jedi.knight@gmail.com>
2024-01-18 20:36:11 +0300

    (add)[internal]{auth models and services}

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"context"
+	"github.com/CodingJediAcademy/go-sso-mservice/internal/domain/models"
+	"log/slog"
+	"time"
+)
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+type Auth struct {
+	log         *slog.Logger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+func New(
+	log *slog.Logger,
+	usrSaver UserSaver,
+	usrProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:         log,
+		usrSaver:    usrSaver,
+		usrProvider: usrProvider,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
+}
