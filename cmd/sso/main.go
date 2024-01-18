c356e6960d0cb0a4040ffbd47f3072b6a2cd3aa7
jedi <coding.jedi.knight@gmail.com>
2024-01-18 17:16:51 +0300

    [add](main){config and logger}

---
+++ cmd/sso/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/CodingJediAcademy/go-sso-mservice/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	// TODO: инициализировать приложение (app)
+
+	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env           string     `yaml:"env" env-default:"local"`
+	StoragePath   string     `yaml:"storagePath" env-required:"true"`
+	GRPC          GRPCConfig `yaml:"GRPC"`
+	MigrationPath string
+	TokenTTL      time.Duration `yaml:"tokenTTL" env-default:"1h"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config path is empty")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("config path is empty: " + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
